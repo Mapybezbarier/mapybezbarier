ad82e2a3237e5c28c667aaee77d5a2abd094bf70
vrbas <vrbas@centrum.cz>
2017-10-28 15:19:24 +0200

    GIS Brno - jine poradi piktogramu

--- app/Module/Web/SourceDetail/BrnoGisSourceDetail.php
+++ app/Module/Web/SourceDetail/BrnoGisSourceDetail.php
@@ -12,6 +12,27 @@ class BrnoGisSourceDetail extends StandardPictogramsSourceDetail
     const DETAIL_DESCRIPTION_URL_MASK = 'http://gis.brno.cz/mapa/assets/local/documents/bezbarierove_objekty/Zdravotnicka_zarizeni_HTML/%s.html';
 
     /**
+     * @var array mapa ciselnikoveho atributu - pristupnost
+     * v podkladech GIS Brno je prohozeno poradi vytah x plosina x rampa
+     */
+    protected $standardPictogramsMap = [
+        13 => 'parking',
+        0 => 'difficult_surface',
+        1 => 'difficult_inclination',
+        // 2 = přístupný hlavním vchodem
+        // 3 = přístupný vedlejším vchodem
+        4 => 'stairs',
+        5 => 'spiral_stairs',
+        7 => 'elevator',
+        8 => 'platform',
+        6 => 'rampskids',
+        9 => 'narrowed_passage',
+        // 10 = WC I.
+        // 11 = WC II.
+        // 12 = WC NOK
+    ];
+
+    /**
      * @param array $object
      *
      * @return array
